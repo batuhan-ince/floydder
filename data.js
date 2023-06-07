552c6e6cfafc341cff1121647b7d068e10a7d1b8
Batuhan İNCE <inceb97@gmail.com>
2023-06-07 12:39:23 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,48 @@
+export const tweetsData = [   
+    {
+        handle: `@DavidGilmour`,
+        profilePic: `images/gilmour.jpg`,
+        likes: 16560,
+        retweets: 10,
+        tweetText: `
+                    And if you see, don't make a sound`,
+        replies: [],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '4b161eee-c0f5-4545-9c4b-8562944223ee',
+    },    
+    {
+        handle: `@RogerWaters`,
+        profilePic: `images/waters.jpg`,
+        likes: 6500,
+        retweets: 234,
+        tweetText: `Scusi dov'e il bar`,
+        replies: [
+                  {
+                handle: `@John ✅`,
+                profilePic: `images/john.jpg`,
+                tweetText: `Say it in English`,
+            },
+                  {
+                handle: `@RogerWaters`,
+                profilePic: `images/waters.jpg`,
+                tweetText: `Oi', where's the heckin' bar John?`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '3c23454ee-c0f5-9g9g-9c4b-77835tgs2',
+    },
+        {
+        handle: `@SydBarrett`,
+        profilePic: `images/barret.jpg`,
+        likes: 10,
+        retweets: 3,
+        tweetText: `I wish I were there..`,
+        replies: [
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '8hy671sff-c0f5-4545-9c4b-1237gyys45',
+    },     
+]
